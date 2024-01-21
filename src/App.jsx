8b411c937c007a1b52e3531634c9439a51bda8f8
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-01-21 14:14:56 -0800

    add NotFound, fix routes and fetch

--- src/App.jsx
+++ src/App.jsx
@@ -12,26 +12,31 @@ const App = () => {
   const [query, setQuery] = useState("cats");
 
   const fetchData = (query) => {
-  fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&page=1&format=json&nojsoncallback=1`)
-  .then(response => response.json())
-  .then(responseData => setPhotos(responseData.photos.photo))
-  .catch(err => console.log("Error fetching and parsing data", err))
+    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&page=1&format=json&nojsoncallback=1`)
+      .then(response => response.json())
+      .then(responseData => setPhotos(responseData.photos.photo))
+      .catch(err => console.log("Error fetching and parsing data", err))
   }
 
-  useEffect(() => {
-    fetchData(query)
-  }, [query]);
+  const handleQuery = (newQuery) => {
+    setQuery(newQuery);
+    fetchData(newQuery);
+  }
+
+  // useEffect(() => {
+  //   fetchData(query)
+  // }, [query]);
 
   return (
     <div>
-      <Search />
+      <Search handleQuery={handleQuery} />
       <Nav />
       <Routes>
         <Route path="/" element={<Navigate to="/cats" />} />
-        <Route path="cats" element={<PhotoList photos={photos} title={'cats'} />} />
-        <Route path="dogs" element={<PhotoList photos={photos} title={'dogs'} />} />
-        <Route path="computers" element={<PhotoList photos={photos} title={'computers'} />} />
-        <Route path="/search/:query" element={<PhotoList photos={photos} />} />
+        <Route path="cats" element={<PhotoList photos={photos} title={'cats'} handleQuery={handleQuery} />} />
+        <Route path="dogs" element={<PhotoList photos={photos} title={'dogs'} handleQuery={handleQuery} />} />
+        <Route path="computers" element={<PhotoList photos={photos} title={'computers'} handleQuery={handleQuery} />} />
+        <Route path="/search/:query" element={<PhotoList photos={photos} handleQuery={handleQuery}/>} />
       </Routes>
     </div>
   )
--- src/components/PhotoList.jsx
+++ src/components/PhotoList.jsx
@@ -1,27 +1,43 @@
-const PhotoList = () => {
+import React, { useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import Photo from "./Photo";
+import NotFound from './NotFound';
+
+const PhotoList = (props) => {
+  const { query } = useParams();
+
+  useEffect(() => {
+    if (query) {
+      props.handleQuery(query);
+    } else {
+      props.handleQuery(props.title)
+    }
+  }, [query, props.title]);
+
+  const foundResults = () => {
+    if (props.photos.length > 0) {
+      return (
+        <ul>
+          {props.photos.map(photo => (
+            <Photo
+              key={photo.id}
+              id={photo.id}
+              server={photo.server}
+              secret={photo.secret}
+            />
+          ))}
+        </ul>
+      );
+    } else {
+      return (<NotFound />);
+    }
+  }
+
   return (
-    <div class="photo-container">
-    <h2>Results</h2>
-    <ul>
-      <li>
-        <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
-      </li>
-      <li>
-        <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />
-      </li>
-      <li>
-        <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
-      </li>
-      <li>
-        <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-      </li>
-      {/* <!-- Not Found --> */}
-      <li class="not-found">
-        <h3>No Results Found</h3>
-        <p>You search did not return any results. Please try again.</p>
-      </li>
-    </ul>
-  </div>
+    <div className="photo-container">
+      <h2>Results</h2>
+      {foundResults()}
+    </div>
   );
 }
 
