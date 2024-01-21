5b5aa5f405d338a25d858ccfd38246b876512bd4
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-01-21 14:18:18 -0800

    remove useEffect in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import apiKey from './config';
 
@@ -22,10 +22,6 @@ const App = () => {
     setQuery(newQuery);
     fetchData(newQuery);
   }
-
-  // useEffect(() => {
-  //   fetchData(query)
-  // }, [query]);
 
   return (
     <div>
